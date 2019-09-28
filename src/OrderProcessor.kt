9e25a5e61efb28cda4b6d90cef6c2bdaf717a131
Jaime <jail81@gmail.com>
2019-09-28 19:46:26 +0200

    Added finish order method implementation.

--- src/OrderProcessor.kt
+++ src/OrderProcessor.kt
@@ -1,6 +1,5 @@
 package challenge
 
-import java.lang.Exception
 import java.util.*
 
 class Order(val customer: Customer, val address: Address) {
@@ -13,7 +12,7 @@ class Order(val customer: Customer, val address: Address) {
         get() = items.sumByDouble { it.total }
 
     fun addProduct(product: Product, quantity: Int) {
-        var productAlreadyAdded = items.any { it.product == product }
+        val productAlreadyAdded = items.any { it.product == product }
         if (productAlreadyAdded)
             throw Exception("The product have already been added. Change the amount if you want more.")
 
@@ -28,7 +27,16 @@ class Order(val customer: Customer, val address: Address) {
             throw Exception("Empty order can not be paid!")
 
         payment = Payment(this, method)
+    }
 
+    fun finish() {
+        if (payment == null) {
+            throw Exception("The order can't be finished if it has not been paid")
+        }
+        if (closedAt != null) {
+            throw Exception("The order has already been closed")
+        }
+        items.forEach { item: OrderItem -> item.product.finish(this.payment!!) }
         close()
     }
 
@@ -57,23 +65,57 @@ data class Invoice(val order: Order) {
     val shippingAddress: Address = order.address
 }
 
-abstract class Product(open val name: String, open val price: Double)
-class PhysicalItem(override val name: String, override val price: Double): Product(name, price)
-class Subscription(override val name: String, override val price: Double): Product(name, price)
-class Book(override val name: String, override val price: Double): Product(name, price)
-class DigitalMedia(override val name: String, override val price: Double): Product(name, price)
+fun printShipmentLabel(address: Address, additionalText: String = "") {}
+fun sendEmail(email: String, text: String) {}
+
+abstract class Product(open val name: String, open val price: Double) {
+    abstract fun finish(payment: Payment)
+}
+
+class PhysicalItem(override val name: String, override val price: Double) : Product(name, price) {
+    override fun finish(payment: Payment) {
+        printShipmentLabel(payment.order.address)
+    }
+}
+
+class Subscription(override val name: String, override val price: Double) : Product(name, price) {
+    var active: Boolean = false
+    override fun finish(payment: Payment) {
+        active = true;
+        sendEmail(payment.order.customer.email, "Your subscription to $name has been activated")
+    }
+}
+
+class Book(override val name: String, override val price: Double) : Product(name, price) {
+    override fun finish(payment: Payment) {
+        printShipmentLabel(payment.order.address, "This is a tax exempt item")
+    }
+}
+
+class DigitalMedia(override val name: String, override val price: Double) : Product(name, price) {
+    override fun finish(payment: Payment) {
+        val currency = "$"
+        sendEmail(
+            payment.order.customer.email,
+            "You have purchased a subscription to $name with a price of $price$currency"
+        )
+        payment.order.customer.discounts.add(DiscountVoucher(10, currency, payment))
+    }
+}
 
 class Address
+class DiscountVoucher(val quantity: Int, val currency: String, val payment: Payment)
+class Customer(val email: String) {
+    var discounts = mutableListOf<DiscountVoucher>()
+}
 
-class Customer
-
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     val shirt = PhysicalItem("Flowered t-shirt", 35.00)
     val netflix = Subscription("Familiar plan", 29.90)
     val book = Book("The Hitchhiker's Guide to the Galaxy", 120.00)
     val music = DigitalMedia("Stairway to Heaven", 5.00)
 
-    val order = Order(Customer(), Address())
+    val order = Order(Customer("order.custome@email.comr"), Address())
 
     order.addProduct(shirt, 2)
     order.addProduct(netflix, 1)
