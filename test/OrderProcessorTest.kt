c2aad66e0cf6e580368440556bad7f54b601d90e
Jaime <jail81@gmail.com>
2019-09-29 21:04:26 +0200

    Added assertion of closing date to completeOrder(). Added mockstatic to check sendEmail() and printShipmentLabel() have been called. Added tests for books and physical items.

--- test/OrderProcessorTest.kt
+++ test/OrderProcessorTest.kt
@@ -1,12 +1,13 @@
 import challenge.*
+import io.mockk.mockkStatic
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-// TODO extract magic numbers to constants
 const val CREDIT_CARD_NUMBER = "43567890-987654367"
-const val CUSTOMER_EMAIL = "order.custome@email.com"
+const val CUSTOMER_EMAIL = "order.customer@email.com"
 
 internal class OrderProcessorTest {
     private val order = Order(Customer(CUSTOMER_EMAIL), Address())
@@ -62,31 +63,59 @@ internal class OrderProcessorTest {
 
     @Nested
     inner class SubmissionRulesTests {
+        @BeforeEach
+        fun setUp() {
+            mockkStatic("challenge.ProductKt")
+        }
+
+        @Test
+        fun `physical item product should print a shipment label after finishing order`() {
+            val shirt = PhysicalItem("Valencia CF century T-shirt", 35.00)
+
+            completeOrder(shirt)
+
+            verify { printShipmentLabel(order.address) }
+        }
+
+        @Test
+        fun `book product should print a shipment label with tax exemption message after finishing order`() {
+            val book = Book("The Expanse: the Leviathan wakes", 20.00)
+
+            completeOrder(book)
+
+            verify { printShipmentLabel(order.address, "This is a tax exempt item") }
+        }
+
         @Test
         fun `subscription product should be active after finishing order`() {
-            val netflix = Subscription("Familiar plan", 29.90)
+            val subscriptionName = "Amazon Prime Video"
+            val amazonPrimeVideo = Subscription(subscriptionName, 29.90)
 
-            completeOrder(netflix)
+            completeOrder(amazonPrimeVideo)
 
-            assertNotNull(order.closedAt)
-            assertTrue(netflix.active)
+            assertTrue(amazonPrimeVideo.active)
+            verify { sendEmail(CUSTOMER_EMAIL, "Your subscription to $subscriptionName has been activated") }
         }
 
         @Test
         fun `digital media product should apply a discount voucher of 10$ after finishing order`() {
-            val music = DigitalMedia("Stairway to Heaven", 5.00)
+            val digitalMediaName = "With or without you"
+            val digitalMediaPrice = 5.00
+            val currency = "$"
+            val music = DigitalMedia(digitalMediaName, digitalMediaPrice)
 
             completeOrder(music)
 
-            assertNotNull(order.closedAt)
             assertNotNull(order.customer.discounts)
             assertEquals(order.customer.discounts.size, 1)
 
             val discountVoucher = order.customer.discounts[0]
 
             assertEquals(discountVoucher.value, 10)
-            assertEquals(discountVoucher.currency, "$")
+            assertEquals(discountVoucher.currency, currency)
             assertEquals(discountVoucher.payment, order.payment)
+
+            verify { sendEmail(CUSTOMER_EMAIL, "You have purchased a subscription to $digitalMediaName with a price of $digitalMediaPrice$currency") }
         }
 
         @Test
@@ -115,6 +144,8 @@ internal class OrderProcessorTest {
             order.addProduct(product, 1)
             order.pay(creditCard)
             order.finish()
+
+            assertNotNull(order.closedAt)
         }
     }
 }
