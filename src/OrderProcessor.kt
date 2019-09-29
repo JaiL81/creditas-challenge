d4d1b3d697b8089bac64fffbbedf2dbc2809c576
Jaime <jail81@gmail.com>
2019-09-29 20:58:39 +0200

    Extracted product hierarchy to its own file.

--- src/OrderProcessor.kt
+++ src/OrderProcessor.kt
@@ -65,44 +65,6 @@ data class Invoice(val order: Order) {
     val shippingAddress: Address = order.address
 }
 
-fun printShipmentLabel(address: Address, additionalText: String = "") {}
-fun sendEmail(email: String, text: String) {}
-
-abstract class Product(open val name: String, open val price: Double) {
-    abstract fun applySubmissionRules(payment: Payment)
-}
-
-class PhysicalItem(override val name: String, override val price: Double) : Product(name, price) {
-    override fun applySubmissionRules(payment: Payment) {
-        printShipmentLabel(payment.order.address)
-    }
-}
-
-class Subscription(override val name: String, override val price: Double) : Product(name, price) {
-    var active: Boolean = false
-    override fun applySubmissionRules(payment: Payment) {
-        active = true
-        sendEmail(payment.order.customer.email, "Your subscription to $name has been activated")
-    }
-}
-
-class Book(override val name: String, override val price: Double) : Product(name, price) {
-    override fun applySubmissionRules(payment: Payment) {
-        printShipmentLabel(payment.order.address, "This is a tax exempt item")
-    }
-}
-
-class DigitalMedia(override val name: String, override val price: Double) : Product(name, price) {
-    override fun applySubmissionRules(payment: Payment) {
-        val currency = "$"
-        sendEmail(
-            payment.order.customer.email,
-            "You have purchased a subscription to $name with a price of $price$currency"
-        )
-        payment.order.customer.discounts.add(DiscountVoucher(10, currency, payment))
-    }
-}
-
 class Address
 class DiscountVoucher(val value: Int, val currency: String, val payment: Payment)
 class Customer(val email: String) {
