3d373f4df3e6e2f5890a3414ff8243aa97fb2e37
Jaime <jail81@gmail.com>
2019-09-29 19:16:11 +0200

    Added tests for digital media and subscriptions orders.

--- test/OrderProcessorTest.kt
+++ test/OrderProcessorTest.kt
@@ -1,17 +1,19 @@
 import challenge.*
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
 // TODO extract magic numbers to constants
 const val CREDIT_CARD_NUMBER = "43567890-987654367"
+const val CUSTOMER_EMAIL = "order.custome@email.com"
+
 internal class OrderProcessorTest {
+    private val order = Order(Customer(CUSTOMER_EMAIL), Address())
+    private val creditCard = CreditCard(CREDIT_CARD_NUMBER)
 
     @Nested
     inner class PaymentTests {
-        private val order = Order(Customer("order.custome@email.comr"), Address())
 
         @BeforeEach
         fun setUp() {
@@ -32,24 +34,26 @@ internal class OrderProcessorTest {
 
         @Test
         fun `payment has been successful`() {
-            order.pay(CreditCard(CREDIT_CARD_NUMBER))
+            order.pay(creditCard)
             assertNotNull(order.payment)
         }
+
         @Test
         fun `duplicate payments on the same order shouldn't be possible`() {
-            order.pay(CreditCard(CREDIT_CARD_NUMBER))
+            order.pay(creditCard)
             assertNotNull(order.payment)
             val exception = assertThrows(Exception::class.java) {
-                order.pay(CreditCard(CREDIT_CARD_NUMBER))
+                order.pay(creditCard)
             }
 
             assertEquals(exception.message, "The order has already been paid!")
         }
+
         @Test
         fun `empty orders shouldn't be paid`() {
-            val emptyOrder = Order(Customer("order.custome@email.comr"), Address())
+            val emptyOrder = Order(Customer(CUSTOMER_EMAIL), Address())
             val exception = assertThrows(Exception::class.java) {
-                emptyOrder.pay(CreditCard(CREDIT_CARD_NUMBER))
+                emptyOrder.pay(creditCard)
             }
 
             assertEquals(exception.message, "Empty order can not be paid!")
@@ -59,7 +63,43 @@ internal class OrderProcessorTest {
     @Nested
     inner class SubmissionRulesTests {
         @Test
-        fun `finish order should have been successful`() {
+        fun `subscription product should be active after finishing order`() {
+            val netflix = Subscription("Familiar plan", 29.90)
+
+            order.addProduct(netflix, 1)
+            order.pay(creditCard)
+            order.finish()
+
+            assertNotNull(order.closedAt)
+            assertTrue(netflix.active)
+        }
+
+        @Test
+        fun `digital media product should apply a discount voucher of 10$ after finishing order`() {
+            val music = DigitalMedia("Stairway to Heaven", 5.00)
+
+            order.addProduct(music, 1)
+            order.pay(creditCard)
+            order.finish()
+
+            assertNotNull(order.closedAt)
+            assertNotNull(order.customer.discounts)
+            assertEquals(order.customer.discounts.size, 1)
+
+            val discountVoucher = order.customer.discounts[0]
+
+            assertEquals(discountVoucher.value, 10)
+            assertEquals(discountVoucher.currency, "$")
+            assertEquals(discountVoucher.payment, order.payment)
+        }
+
+        @Test
+        fun `empty orders shouldn't be finished`() {
+
+        }
+
+        @Test
+        fun `an already closed order shouldn't be finished again`() {
 
         }
     }
